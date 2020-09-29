aaf3ee70640959206197a35dd8e5d71b334f2c70
hajdinim <hajdinim@uni.coventry.ac.uk>
2020-09-30 00:07:30 +0100

    Initial Commit

---
+++ LibraryItem/LibraryItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryItemApp.swift
+//  LibraryItem
+//
+//  Created by Tony Hajdini on 30/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct LibraryItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
